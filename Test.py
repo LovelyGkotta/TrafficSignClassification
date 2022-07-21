cade8a6cf475a338569208f82bebad00165173d8
陈昱杰 <chenyujie@chenyujies-MacBook-Pro.local>
2022-07-22 03:37:14 +0900

    Adapt to macOS

--- Test.py
+++ Test.py
@@ -21,46 +21,26 @@ def preprocessing(img):  # 画像前処理の関数
 
 
 def getClassName(classNo):  # クラス
-    if classNo == 0:
-        return '通行止め'
-    elif classNo == 1:
-        return '自転車通行止め'
-    elif classNo == 2:
-        return '駐車禁止'
-    elif classNo == 3:
-        return '一方通行(左)'
-    elif classNo == 4:
-        return '一方通行(前)'
-    elif classNo == 5:
-        return '駐車可'
-    elif classNo == 6:
-        return '横断歩道'
-    elif classNo == 7:
-        return '優先道路'
-    elif classNo == 8:
-        return '道路工事中'
-    elif classNo == 9:
-        return '環状の交差点'
-    elif classNo == 10:
-        return '徐行'
-    elif classNo == 11:
-        return '最高速度40'
-    elif classNo == 12:
-        return '最高速度50'
-    elif classNo == 13:
-        return '一時停止'
-    elif classNo == 14:
-        return '直進以外進行禁止'
-    elif classNo == 15:
-        return '信号機あり'
-    elif classNo == 16:
-        return '左折以外進行禁止'
-    elif classNo == 17:
-        return '右折以外進行禁止'
-    elif classNo == 18:
-        return '二方向交通'
-    elif classNo == 19:
-        return '車両進入禁止'
+    if classNo == 0: return '通行止め'
+    elif classNo == 1: return '自転車通行止め'
+    elif classNo == 2: return '駐車禁止'
+    elif classNo == 3: return '一方通行(左)'
+    elif classNo == 4: return '一方通行(前)'
+    elif classNo == 5: return '駐車可'
+    elif classNo == 6: return '横断歩道'
+    elif classNo == 7: return '優先道路'
+    elif classNo == 8: return '道路工事中'
+    elif classNo == 9: return '環状の交差点'
+    elif classNo == 10: return '徐行'
+    elif classNo == 11: return '最高速度40'
+    elif classNo == 12: return '最高速度50'
+    elif classNo == 13: return '一時停止'
+    elif classNo == 14: return '直進以外進行禁止'
+    elif classNo == 15: return '信号機あり'
+    elif classNo == 16: return '左折以外進行禁止'
+    elif classNo == 17: return '右折以外進行禁止'
+    elif classNo == 18: return '二方向交通'
+    elif classNo == 19: return '車両進入禁止'
 
 
 # 日本語描画（cv2.putTextは日本語を使えない）
@@ -69,8 +49,10 @@ def addText(img, text, left, top, textcolor=(0, 255, 0), textsize=50):
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         draw = ImageDraw.Draw(img)
         fontText = ImageFont.truetype(
-            # on linux /usr/share/fonts/opentype/noto/NotoSansCJK-Ragular.ttc
-            "font/simsun.ttc", textsize, encoding="utf-8")
+            # linux  /usr/share/fonts/opentype/noto/NotoSansCJK-Ragular.ttc
+            # windows  font/simsun.ttc
+            # Macos  ~/Library/Fonts/NotoSansCJKjp-Regular.ttf
+            "~/Library/Fonts/NotoSansCJKjp-Regular.ttf", textsize, encoding="utf-8")
         draw.text((left, top), text, textcolor, font=fontText)
         return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
